d6982286bed5f050002edcbd6abe29032a92f8e3
Gxtoxic4563 <gxtoxic211@gmail.com>
2026-05-16 19:35:52 +0530

    Update handlers.py

--- modules/handlers.py
+++ modules/handlers.py
@@ -395,7 +395,8 @@ async def callbacks(_, callback: CallbackQuery):
 
     elif callback.data == "intro_back":
 
-    return await callback.message.edit_caption(
+        return await 
+    callback.message.edit_caption(
         caption="""
 ✨ **𝗥𝘂𝗵𝗶 𝗦𝘂𝗽𝗿𝗲𝗺𝗲 𝗔𝗜 💫**
 
